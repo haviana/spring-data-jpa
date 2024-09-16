7ee44faa2736bc76e3dca3edb57f783736943186
Hugo Viana <extern.viana_hugo@allianz.de>
2024-09-16 16:12:10 +0100

    initial commit

---
+++ src/main/java/amigos/com/springdatajpacourses/SpringDataJpaCoursesApplication.java
@@ -0,0 +1,13 @@
+package amigos.com.springdatajpacourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaCoursesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/amigos/com/springdatajpacourses/SpringDataJpaCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package amigos.com.springdatajpacourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
